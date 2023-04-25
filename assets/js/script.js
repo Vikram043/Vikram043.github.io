87b994054001858a039211dfa5b2486f3145eb18
Vikram Naik <knowme962@gmail.com>
2023-04-25 17:00:18 +0530

    portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,33 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
+const resumeButtons = document.querySelectorAll('.rsd');
+const resumeLink = 'https://drive.google.com/file/d/1T1fb-o2b3NbCVBClb4TS7VDRRief5Lha/view?usp=share_link';
+
+resumeButtons.forEach(btn => {
+  btn.addEventListener('click', e => {
+    e.preventDefault();
+    window.open(resumeLink, '_blank');
+    // Download PDF file
+   download(resumeLink)
+  });
+});
+
+function download(resumeLink) {
+  console.log("hi")
+  const a = document.createElement('a')
+  a.href = resumeLink
+  a.download = "file.pdf"
+  a.click()
+  
+}
+
+
+document.getElementById("download-btn").addEventListener("click", function() {
+ // window.location.href = "https://drive.google.com/uc?id=1pQG8MxY_9_EEX_f1_jKrBqzPplnfYLJa";
+  var link = document.createElement("a");
+  link.href = "https://drive.google.com/uc?id=1pQG8MxY_9_EEX_f1_jKrBqzPplnfYLJa";
+  link.download = "filename.pdf"; // Replace "filename.pdf" with the name you want to give to the downloaded file
+  link.click();
+});
+
